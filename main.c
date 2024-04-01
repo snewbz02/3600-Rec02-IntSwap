7c547b9f81ca625408131f7ba1527b621f4b329c
Sienna Newby-Sanchez <113070105+snewbz02@users.noreply.github.com>
2024-04-01 16:32:41 -0500

    Create main.c / description
    Added assignment goal/description

---
+++ main.c
@@ -0,0 +1,11 @@
+/*
+Created by Sienna Newby-Sanchez
+
+Goal: Create a C program that accepts any two integers
+and swaps both integers using bitwise operators without a third variable.
+
+We will be using the bitwise XOR operator to swap the integers as the bitwise XOR
+operator evaluates each bit of the result to 1. 
+
+Class: 3600.004-208
+*/
